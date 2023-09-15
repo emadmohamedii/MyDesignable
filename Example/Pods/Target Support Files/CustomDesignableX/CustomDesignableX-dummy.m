307ca37678f91241741857df26a7df4610e1548c
emad habib <ehabib@sarmady.net>
2023-09-15 16:06:39 +0300

    changes

---
+++ Example/Pods/Target Support Files/CustomDesignableX/CustomDesignableX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CustomDesignableX : NSObject
+@end
+@implementation PodsDummy_CustomDesignableX
+@end
---
+++ Example/Pods/Target Support Files/Pods-CustomDesignableX_Tests/Pods-CustomDesignableX_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomDesignableX_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CustomDesignableX_Tests
+@end
